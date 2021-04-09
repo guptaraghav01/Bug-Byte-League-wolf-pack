5e9d9e3f613798bcdf18d33a9e17b63f65caaf20
guptaraghav01 <raghavgupta0112@gmail.com>
2021-04-09 10:57:28 +0530

    final changes

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -3,23 +3,25 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flask_login import current_user
 
+
 class RegistrationForm(FlaskForm):
     FirstName = StringField('First Name',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                            validators=[DataRequired(), Length(min=2, max=20)])
     MiddleName = StringField('Middle Name')
     LastName = StringField('Last Name',
                            validators=[DataRequired(), Length(min=2, max=20)])
     DOB = StringField('Date of Birth',
-                           validators=[DataRequired()])
+                      validators=[DataRequired()])
     UserEmail = StringField('E-Mail',
-                           validators=[DataRequired(), Email()])
+                            validators=[DataRequired(), Email()])
     PhoneNo = StringField('Phone Number',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                          validators=[DataRequired(), Length(min=2, max=20)])
     Education = StringField('Name of School/College/Company',
-                           validators=[DataRequired(), Length(min=2, max=60)])
-    type = StringField('You are here as: (Student/Professional)',
                             validators=[DataRequired(), Length(min=2, max=60)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
+    type = StringField('You are here as: (Learner/Professional)',
+                       validators=[DataRequired(), Length(min=2, max=60)])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=6, max=20)])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
@@ -28,32 +30,35 @@ class RegistrationForm(FlaskForm):
     def validate_email(self, email):
         user = User.query.filter_by(UserEmail=UserEmail.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+            raise ValidationError(
+                'That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
     UserEmail = StringField('Email',
-                        validators=[DataRequired(), Email()])
+                            validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+
 class UpdateAccountForm(FlaskForm):
     FirstName = StringField('First Name',
-                           validators=[Length(min=2, max=20)])
+                            validators=[Length(min=2, max=20)])
     MiddleName = StringField('Middle Name')
     LastName = StringField('Last Name',
                            validators=[Length(min=2, max=20)])
     DOB = StringField('Date of Birth')
     UserEmail = StringField('E-Mail')
     PhoneNo = StringField('Phone Number',
-                           validators=[Length(min=2, max=20)])
+                          validators=[Length(min=2, max=20)])
     Education = StringField('Name of School/College/Company',
-                           validators=[Length(min=2, max=60)])
+                            validators=[Length(min=2, max=60)])
     submit = SubmitField('Save Changes')
 
     def validate_email(self, email):
         if UserEmail.data != current_user.UserEmail:
             user = User.query.filter_by(UserEmail=UserEmail.data).first()
             if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
+                raise ValidationError(
+                    'That email is taken. Please choose a different one.')
